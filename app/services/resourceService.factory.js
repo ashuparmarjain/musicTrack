394235c74e204765e10700a79a94cf8140bfd2e9
Ashu parmar <Ashu parmar>
2017-05-13 13:14:27 +0530

    small setup change

--- app/services/resourceService.factory.js
+++ app/services/resourceService.factory.js
@@ -1,6 +1,6 @@
 app.factory("MusicTrack", function ($resource) {
     return $resource(
-        "http://104.197.128.152:8000/v1/tracks/:id",
+        "//104.197.128.152:8000/v1/tracks/:id",
         {page: "@page",id:'@id'},
         {
             "update": {method: "POST"},
@@ -12,7 +12,7 @@ app.factory("MusicTrack", function ($resource) {
 
 app.factory("GenreList", function ($resource) {
     return $resource(
-        "http://104.197.128.152:8000/v1/genres",
+        "//104.197.128.152:8000/v1/genres",
         {page: "@page",id:'@id'},
         {
             "update": {method: "POST"},
